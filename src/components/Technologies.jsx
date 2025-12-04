212fd5fa7deaeeff02f7cf7b95f1309ddb3ddebd
Nikhilesh Mauje <maujenikhilesh@gmail.com>
2025-12-04 22:22:51 +0530

    Add new Technologies in Skill Section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,12 +1,11 @@
-import { RiReactjsLine,RiTailwindCssFill } from "react-icons/ri";
+import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
 import { TiHtml5 } from "react-icons/ti";
-import { BiLogoPostgresql} from "react-icons/bi";
+import { BiLogoPostgresql } from "react-icons/bi";
 import { FaNode } from "react-icons/fa6";
-import { FaGitSquare, FaGithubSquare } from "react-icons/fa";
+import { FaGitSquare, FaGithubSquare, FaDocker } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
 import { TbBrandCpp } from "react-icons/tb";
-import { SiPostman } from "react-icons/si";
-
+import { SiPostman, SiMongodb, SiRabbitmq } from "react-icons/si";
 
 const Technologies = () => {
   return (
@@ -15,48 +14,72 @@ const Technologies = () => {
         Technologies
       </h2>
       <div className="h-1 w-20 bg-white mb-8"></div>
-      <div className="grid grid-cols-3 md:grid-cols-5 gap-1">
-        <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="flex flex-col items-center">
-          <RiReactjsLine className="text-6xl text-cyan-400" />
-          <p>React Js</p>
-        </div>
-        </div>
+
+      <div className="grid grid-cols-3 md:grid-cols-5 gap-6">
+
         <div className="p-4 flex flex-col items-center">
-          <RiTailwindCssFill  className="text-6xl text-blue-600" />
-          <p >Tailwind Css</p>
+          <RiReactjsLine className="text-6xl text-cyan-400" />
+          <p>React JS</p>
         </div>
+
+        <div className="p-4 flex flex-col items-center">
+          <RiTailwindCssFill className="text-6xl text-blue-600" />
+          <p>Tailwind CSS</p>
+        </div>
+
         <div className="p-4 flex flex-col items-center">
           <TiHtml5 className="text-6xl text-orange-600" />
           <p>HTML 5</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
           <BiLogoPostgresql className="text-6xl text-blue-950" />
-          <p>PostgresSQL</p>
+          <p>PostgreSQL</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-            <FaNode  className="text-6xl text-green-600" />
-            <p>Node Js</p>
+          <FaNode className="text-6xl text-green-600" />
+          <p>Node JS</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-          <FaGitSquare  className="text-6xl text-orange-600" />
+          <FaGitSquare className="text-6xl text-orange-600" />
           <p>Git</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-            <FaGithubSquare className="text-6xl" />
-            <p>Github</p>
+          <FaGithubSquare className="text-6xl" />
+          <p>GitHub</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-            <IoLogoJavascript className="text-6xl text-yellow-600"/> 
-            <p>Javascript</p>
+          <IoLogoJavascript className="text-6xl text-yellow-600" />
+          <p>JavaScript</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-            <TbBrandCpp className="text-6xl text-blue-950"/> 
-            <p>C++</p>
+          <TbBrandCpp className="text-6xl text-blue-950" />
+          <p>C++</p>
         </div>
+
         <div className="p-4 flex flex-col items-center">
-            <SiPostman className="text-6xl text-orange-600"/> 
-            <p>Postman API</p>
+          <SiPostman className="text-6xl text-orange-600" />
+          <p>Postman</p>
+        </div>
+
+        <div className="p-4 flex flex-col items-center">
+          <SiMongodb className="text-6xl text-green-700" />
+          <p>MongoDB</p>
+        </div>
+
+        <div className="p-4 flex flex-col items-center">
+          <SiRabbitmq className="text-6xl text-orange-500" />
+          <p>RabbitMQ</p>
+        </div>
+
+        <div className="p-4 flex flex-col items-center">
+          <FaDocker className="text-6xl text-blue-500" />
+          <p>Docker</p>
         </div>
 
       </div>
